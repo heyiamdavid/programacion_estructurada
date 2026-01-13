c05ce5464648d55f599e3777298f64ee1c163496
heyiamdavid522 <heyiamdavid19@gmail.com>
2026-01-13 18:11:51 -0500

    proyecto_progamacion

---
+++ generator_qr.py
@@ -0,0 +1,14 @@
+import qrcode
+
+texto = ""
+
+qr = qrcode.QRCode(version=1,
+                    box_size=10,
+                    border=2)
+qr.add_data(texto)
+qr.make(fit=True)
+
+imagen_qr = qr.make_image(fill_color="black",
+                          back_color="white")
+
+imagen_qr.save("qr_image/codigo_qr.png")                          
